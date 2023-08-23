6042f587c977dcb28faafca637d2ef18d7d3ba3a
barakus <diegocallediraldo@gmail.com>
2023-08-23 11:08:33 +0200

    test23

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-# let init = 
+let message_one = ["you are having ", "you need ", "you will have tomorrow ", "try to have "]
+let message_two = ["a nice day", "a bad day", "a lucky day", "a regular day", "a sad day", "a funny day"]
+
+// generate random numbers
+
+let random_n_one = Math.floor(Math.random()*message_one.length);
+let random_n_two = Math.floor(Math.random()*message_two.length);
+
+console.log (message_one[random_n_one] + message_two[random_n_two])
